b1956677b0433eb20e46e25de6924073c8ec0862
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-08-17 21:11:29 +0600

    Initial Commit

---
+++ OpenMedicine/OpenMedicineApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenMedicineApp.swift
+//  OpenMedicine
+//
+//  Created by Apple on 17/8/21.
+//
+
+import SwiftUI
+
+@main
+struct OpenMedicineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
