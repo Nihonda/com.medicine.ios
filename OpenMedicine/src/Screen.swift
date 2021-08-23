2b808f5b787b764074461cbe99e50d5f67b693d8
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-08-23 22:42:01 +0600

    Remove unrelated objects from Screen.swift

--- OpenMedicine/src/Screen.swift
+++ OpenMedicine/src/Screen.swift
@@ -11,18 +11,6 @@ class Screen {
     static let width = UIScreen.main.bounds.width
     static let height = UIScreen.main.bounds.height
     
-    class Home {
-        static let HORIZONTAL_PADDING = CGFloat(15)
-    }
-    
-    class Keep {
-        static let HORIZONTAL_PADDING = CGFloat(15)
-    }
-    
-    class ResultCell {
-        static let HORIZONTAL_PADDING = CGFloat(10)
-    }
-    
     // device flags
     static var isIpadDevice: Bool {
         return (UIDevice.current.userInterfaceIdiom == .pad)
