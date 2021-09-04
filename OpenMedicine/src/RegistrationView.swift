6bb0ab06284bee49ec194f96a734880b2fc3e666
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-09-05 02:02:51 +0600

    Add gender to Registration screen

--- OpenMedicine/src/RegistrationView.swift
+++ OpenMedicine/src/RegistrationView.swift
@@ -26,9 +26,26 @@ struct RegistrationView: View {
     
     // gender
     @State private var genderBinding: String = ""
-    @State private var isGenderFocused = false
     @State private var isGenderError: Bool = false
     @State private var showGenderPicker: Bool = false
+    
+    // birthday
+    var dateClosedRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -99, to: Date())!
+        let max = Calendar.current.date(byAdding: .year, value: -15, to: Date())!
+        return min...max
+    }
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.locale = Locale.init(identifier: "ru_RU")
+        formatter.dateFormat = "YYYY-MM-dd"
+        return formatter
+    }
+    @State private var birthdayBinding: String = ""
+    @State private var birthDate = Date()
+    @State private var showBirthdayPicker = false
+    @State private var isBirthdayError: Bool = false
 
     var body: some View {
         ZStack(alignment: .leading) {
@@ -88,7 +105,6 @@ struct RegistrationView: View {
                     HStack(alignment:.top, spacing: 0) {
                         VStack(alignment: .leading, spacing: 0) {
                             TextField("Пол", text: self.$genderBinding.onChange(onGenderChanged), onEditingChanged: { editingChanged in
-                                isGenderFocused = editingChanged
                                 if editingChanged {
                                     // focused
                                     
@@ -109,7 +125,7 @@ struct RegistrationView: View {
                                             .strokeBorder(Color.black, style: StrokeStyle(lineWidth: 0.5))
                                     }
                                 )
-                                .background(RoundedRectangle(cornerRadius: 10.0).fill(isEmailError ? Color(red: 0.93, green: 0.74, blue: 0.71, opacity: 1.0) : isGenderFocused || !genderBinding.isEmpty ? Color.white : Color(red: 230/255, green: 236/255, blue: 239/255)))
+                                .background(RoundedRectangle(cornerRadius: 10.0).fill(isEmailError ? Color(red: 0.93, green: 0.74, blue: 0.71, opacity: 1.0) : !genderBinding.isEmpty ? Color.white : Color(red: 230/255, green: 236/255, blue: 239/255)))
                             
                             VStack {
                                 if isGenderError {
@@ -127,6 +143,46 @@ struct RegistrationView: View {
                     .onTapGesture {
                         self.showGenderPicker = true
                     }
+                    
+                    // date of birth
+                    HStack(alignment:.top, spacing: 0) {
+                        VStack(alignment: .leading, spacing: 0) {
+                            TextField("Дата рождения", text: self.$birthdayBinding.onChange(onBirthdayChanged), onEditingChanged: { editingChanged in
+                                if editingChanged {
+                                    // focused
+                                    
+                                } else {
+                                    // focus lost
+                                    
+                                }
+                            })
+                                .disableAutocorrection(true)
+                                .disabled(true)
+                                .font(Font.system(size: 16))
+                                .frame(height: 30)
+                                .padding(7)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10.0)
+                                        .strokeBorder(Color.black, style: StrokeStyle(lineWidth: 0.5))
+                                )
+                                .background(RoundedRectangle(cornerRadius: 10.0).fill(isBirthdayError ? Color(red: 0.93, green: 0.74, blue: 0.71, opacity: 1.0) : !birthdayBinding.isEmpty ? Color.white : Color(red: 230/255, green: 236/255, blue: 239/255)))
+                            
+                            VStack {
+                                if isBirthdayError {
+                                    Text("Проверьте правильность")
+                                        .font(.system(size: 12))
+                                        .fontWeight(.light)
+                                        .foregroundColor(.red)
+                                        .padding(.leading)
+                                }
+                            }
+                            .frame(height: 14)
+                        }
+                        .onTapGesture {
+                            self.showBirthdayPicker = true
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
                 }
                 
                 Spacer()
@@ -172,6 +228,39 @@ struct RegistrationView: View {
                     }
                 }
             }
+            
+            if showBirthdayPicker {
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+
+                        Button(action: {
+                            showBirthdayPicker = false
+                        }) {
+                            Text("Закрыть")
+                        }
+                        .padding([.horizontal, .vertical], 10)
+                    }
+
+                    DatePicker(
+                        "Please choose date",
+                        selection: $birthDate,
+                        in: dateClosedRange,
+                        displayedComponents: .date
+                    )
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                        .environment(\.locale, Locale.init(identifier: "ru_RU"))
+                        .onAppear {
+                            // self.dismissKeyboard()
+                        }
+                        .onChange(of: birthDate) { value in
+                            birthdayBinding = dateFormatter.string(from: value)
+                        }
+                }
+            }
         }
         .frame(width: Screen.width)
     }
@@ -194,5 +283,9 @@ extension RegistrationView {
     
     private func onGenderChanged(_ text: String) {
 
+    }
+    
+    private func onBirthdayChanged(_ text: String) {
+        
     }
 }
