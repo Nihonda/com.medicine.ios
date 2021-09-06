ea0e43c12a2852b6bb85f005b8212d589835e428
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-09-06 22:36:28 +0600

    WIP: Change CoateItem from struct to class to be alterable

--- OpenMedicine/src/CoateData.swift
+++ OpenMedicine/src/CoateData.swift
@@ -11,11 +11,28 @@ struct CoateData: Codable {
     var data: CoateItem
 }
 
-struct CoateItem: Codable {
+class CoateItem: Codable, Identifiable {
+    var id: Int {
+        return cd
+    }
     let cd: Int
     let nm: String
     let code: String
     let capital_name: String?
     let capital_code: String?
     let child: [CoateItem]?
+    
+    var selected: Bool
+    var level: Int
+    
+    init(cd: Int, nm: String, code: String, capital_name: String?, capital_code: String?, child: [CoateItem]?, selected: Bool, level: Int) {
+        self.cd = cd
+        self.nm = nm
+        self.code = code
+        self.capital_name = capital_name
+        self.capital_code = capital_code
+        self.child = child
+        self.selected = selected
+        self.level = level
+    }
 }
