40a0f789921b30ca5515d8e7451e419b91953b3a
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-10-23 23:00:03 +0600

    Update checkbox event on CoateView

--- OpenMedicine/src/CoateView.swift
+++ OpenMedicine/src/CoateView.swift
@@ -10,15 +10,33 @@ import SwiftUI
 struct CoateView: View {
     @EnvironmentObject var coate: CoateViewModel
     
+    @State var isUpdated = true
+    
     var body: some View {
         VStack {
-            if let coate = self.coate.data.data {
-                List{
-                    OutlineGroup(coate, children: \.child) { item in
-                        CoateCell(item: item)
+            if isUpdated {
+                if let parent = coate.data.data, let children = parent.child, children.count > 0 {
+                    List{
+                        OutlineGroup(parent, children: \.child) { item in
+                            HStack(spacing: 15) {
+                                Image(systemName: item.selected == true ? "checkmark.circle.fill" : "circle")
+                                    .resizable()
+                                    .renderingMode(.original)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: 30, alignment: .center)
+                                    .onTapGesture {
+                                        triggerUpdate(for: item)
+                                    }
+
+                                Text(item.nm)
+                                    .font(Font.system(size: 14))
+                                
+                                Spacer()
+                            }
+                        }
                     }
+                    .listStyle(PlainListStyle())
                 }
-                .listStyle(PlainListStyle())
             }
             
             Spacer()
@@ -33,10 +51,21 @@ struct CoateView: View {
 
 struct CoateView_Previews: PreviewProvider {
     static var previews: some View {
-        CoateView()
+        NavigationView {
+            CoateView()
+        }
+        .environmentObject(CoateViewModel())
     }
 }
 
 extension CoateView {
-    
+    private func triggerUpdate(for item: CoateItem) {
+        item.selected?.toggle() ?? (item.selected = true)
+        
+        // hide/show OutlineGroup to trigger update event
+        isUpdated = false
+        DispatchQueue.main.async {
+            self.isUpdated = true
+        }
+    }
 }
