2c1c2d0596f36708e4809c6351088bd87d255b23
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-11-08 21:33:27 +0600

    Add Combine to Api shared and add downloadNumberData() func

--- OpenMedicine/src/Api.swift
+++ OpenMedicine/src/Api.swift
@@ -6,10 +6,17 @@
 //
 
 import Foundation
+import Combine
 
 class Api {
     static let shared = Api()
     
+    @Published var numberModel: NumberModel = NumberModel(numOf: NumberCount(count: 0))
+    var cancellables = Set<AnyCancellable>()
+    
+    private init() {} // avoid creating object
+    
+    // GENERIC FUNCTION
     typealias ResultBlock<T> = (Result <T, Error>) -> Void
     
     func fetch<T: Decodable>(of type: T.Type,
@@ -35,5 +42,35 @@ class Api {
             }
             .resume()
         }
+    }
+    
+    // FROM https://www.youtube.com/watch?v=fmVuOu8XOvQ&list=PLwvDm4VfkdpiagxAXCT33Rkwnc5IVhTar&index=29
+    func downloadNumberData() {
+        guard let url = URL(string: K.API.NUMBER_TOTAL) else { return }
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .tryMap(handleOutput)
+            .decode(type: NumberModel.self, decoder: JSONDecoder())
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error downloading data. \(error.localizedDescription)")
+                }
+            } receiveValue: { [weak self] (returnedNumberModel) in
+                self?.numberModel = returnedNumberModel
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func handleOutput(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard
+            let response = output.response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode <= 300 else {
+                throw URLError(.badServerResponse)
+            }
+        return output.data
     }
 }
