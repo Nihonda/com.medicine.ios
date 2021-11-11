538d41eb7a1bc5d95d8cb3986ec313237b978494
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-11-11 23:13:09 +0600

    Create DrugListModel

---
+++ OpenMedicine/src/DrugListModel.swift
@@ -0,0 +1,50 @@
+//
+//  DrugListViewModel.swift
+//  OpenMedicine
+//
+//  Created by Nurlan Nihonda on 11/11/21.
+//
+
+import Foundation
+
+// MARK: - DrugListModel
+struct DrugListModel: Codable {
+    let items: [Item]
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let atc: Atc?
+    let barcode: String
+    let expiryDate: String?
+    let fullName: String?
+    let maker, status: Maker?
+    let issueDate: String?
+    let pricePerUnit: Double?
+    let medicineFormula: String?
+    let thumbName: String?
+
+    enum CodingKeys: String, CodingKey {
+        case atc, barcode
+        case expiryDate = "expiry_date"
+        case fullName = "full_name"
+        case maker, status
+        case issueDate = "issue_date"
+        case pricePerUnit = "price_per_unit"
+        case medicineFormula = "medicine_formula"
+        case thumbName = "thumb_name"
+    }
+}
+
+// MARK: - Atc
+struct Atc: Codable {
+    let cd: Int
+    let nm: String
+    let nmEng: String?
+}
+
+// MARK: - Maker
+struct Maker: Codable {
+    let cd: Int
+    let nm: String
+}
