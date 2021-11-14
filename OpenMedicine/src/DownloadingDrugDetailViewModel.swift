ccf359e9af8a10aff972677181f0a56de2e60315
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-11-15 00:16:41 +0600

    Create DrugDetailViewModel

---
+++ OpenMedicine/src/DownloadingDrugDetailViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  DownloadingDrugDetailViewModel.swift
+//  OpenMedicine
+//
+//  Created by Nurlan Nihonda on 15/11/21.
+//
+
+import Foundation
+import Combine
+
+class DownloadingDrugDetailViewModel: ObservableObject {
+    
+    @Published var drugDetailItem: DrugDetailModel? = nil
+    var cancellables = Set<AnyCancellable>()
+    
+    let dataService = Api.shared
+    
+    init() {  }
+    
+    func downloadData(barcode: String) {
+        dataService.downloadDrugDetailData(barcode: barcode)
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        dataService.$drugDetailItem
+            .sink { [weak self] (returnedDrugDetailItem) in
+                self?.drugDetailItem = returnedDrugDetailItem
+            }
+            .store(in: &cancellables)
+    }
+    
+    func clear() {
+        drugDetailItem = nil
+    }
+}
