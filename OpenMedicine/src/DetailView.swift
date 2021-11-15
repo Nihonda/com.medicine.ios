a8d3b570385059a602a5e3c7e0c986a3ad4f0964
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-11-15 18:33:08 +0600

    Add bottom buttons to DetailView

--- OpenMedicine/src/DetailView.swift
+++ OpenMedicine/src/DetailView.swift
@@ -14,64 +14,107 @@ struct DetailView: View {
     let barcode: String
     
     var body: some View {
-        ScrollView {
-            VStack {
-                if let name = vm.drugDetailItem?.detail.fullName {
-                    Text(name)
-                        .font(.system(size: 15))
-                        .fontWeight(.bold)
-                }
-                
-                if let url = vm.drugDetailItem?.detail.thumbName {
-                    DownloadingImageView(url: url, key: "1")
-                        .frame(maxWidth: Screen.width * 3/4)
-                }
-                
-                shortIntro
-                    .frame(maxWidth: .infinity)
-                
-                // https://www.vidal.ru/drugs/5-nok__5470
-                if let instruction = vm.drugDetailItem?.detail.instruction {
-                    textContainer("Показания к применению", body: instruction)
-                }
-                
-                if let contraIndication = vm.drugDetailItem?.detail.contraIndication {
-                    textContainer("Противопоказания к применению", body: contraIndication)
-                }
-                
-                if let dose = vm.drugDetailItem?.detail.dose {
-                    textContainer("Режим дозирования", body: dose)
-                }
-                
-                if let sideEffect = vm.drugDetailItem?.detail.sideEffect {
-                    textContainer("Побочное действие", body: sideEffect)
-                }
-                
-                if let overdose = vm.drugDetailItem?.detail.overdose {
-                    textContainer("Дополнительно", body: overdose)
-                }
-            }
-            .padding()
-        }
-        .onAppear {
-            vm.downloadData(barcode: barcode)
-        }
-        .navigationBarTitle("Подробно", displayMode: .inline)
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(
-            leading:
-                Button(action: {
-                    presentationMode.wrappedValue.dismiss()
-                }, label: {
-                    HStack {
-                        Image(systemName: "arrow.backward")
-                            .resizable()
-                            .renderingMode(.original)
-                            .aspectRatio(contentMode: .fit)
-                        Text("Назад")
+        ZStack {
+            bottomButtons
+                .padding(.bottom)
+                .zIndex(1)
+            
+            ScrollView {
+                VStack {
+                    if let name = vm.drugDetailItem?.detail.fullName {
+                        Text(name)
+                            .font(.system(size: 15))
+                            .fontWeight(.bold)
                     }
-                })
-        )
+                    
+                    if let url = vm.drugDetailItem?.detail.thumbName {
+                        DownloadingImageView(url: url, key: "1")
+                            .frame(maxWidth: Screen.width * 3/4)
+                    }
+                    
+                    shortIntro
+                        .frame(maxWidth: .infinity)
+                    
+                    // https://www.vidal.ru/drugs/5-nok__5470
+                    if let instruction = vm.drugDetailItem?.detail.instruction {
+                        textContainer("Показания к применению", body: instruction)
+                    }
+                    
+                    if let contraIndication = vm.drugDetailItem?.detail.contraIndication {
+                        textContainer("Противопоказания к применению", body: contraIndication)
+                    }
+                    
+                    if let dose = vm.drugDetailItem?.detail.dose {
+                        textContainer("Режим дозирования", body: dose)
+                    }
+                    
+                    if let sideEffect = vm.drugDetailItem?.detail.sideEffect {
+                        textContainer("Побочное действие", body: sideEffect)
+                    }
+                    
+                    if let overdose = vm.drugDetailItem?.detail.overdose {
+                        textContainer("Дополнительно", body: overdose)
+                    }
+                }
+                .padding()
+                .padding(.bottom, 60)
+            }
+            .onAppear {
+                vm.downloadData(barcode: barcode)
+            }
+            .navigationBarTitle("Подробно", displayMode: .inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(
+                leading:
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        HStack {
+                            Image(systemName: "arrow.backward")
+                                .resizable()
+                                .renderingMode(.original)
+                                .aspectRatio(contentMode: .fit)
+                            Text("Назад")
+                        }
+                    })
+            )
+        }
+    }
+    
+    private var bottomButtons: some View {
+        VStack {
+            Spacer()
+            
+            HStack {
+                Button(action: {}) {
+                    Text("Пожаловаться")
+                        .foregroundColor(Color(.systemRed))
+                }
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20).stroke(Color(.systemRed), lineWidth: 2)
+                )
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color(red: 246/255, green: 251/255, blue: 255/255))
+                )
+                
+                Button(action: {}) {
+                    Text("Показать на карте")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBackground))
+                }
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20).stroke(Color(.systemBackground), lineWidth: 2)
+                )
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color(red: 0, green: 164/255, blue: 255/255))
+                )
+            }
+            .padding(.horizontal, 20)
+        }
     }
     
     private var shortIntro: some View {
