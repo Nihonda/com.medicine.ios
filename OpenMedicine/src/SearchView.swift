a108dc469a61c9576a75edcbe73123332d53db52
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-12-15 00:58:47 +0600

    Add sheet views of Mnn, atc, country as popup in SearchView

--- OpenMedicine/src/SearchView.swift
+++ OpenMedicine/src/SearchView.swift
@@ -43,10 +43,10 @@ struct SearchView_Previews: PreviewProvider {
 extension SearchView {
     private func initItems() {
         items = [
-            SearchItem(icon: "paperplane", label: "Страна производителя", binding: $countryBinding, isPresented: $countryModal, handler: tapCountry),
-            SearchItem(icon: "number", label: "МНН", binding: $mnnBinding, isPresented: $mnnModal, handler: tapMNN),
-            SearchItem(icon: "pills", label: "Лекарственная форма", binding: $formBinding, isPresented: $formModal, handler: tapForm),
-            SearchItem(icon: "eye", label: "АТХ", binding: $atcBinding, isPresented: $atcModal, handler: tapATC),
+            SearchItem(icon: "paperplane", label: "Страна производителя", modalType: .country, binding: $countryBinding, isPresented: $countryModal, handler: tapCountry),
+            SearchItem(icon: "number", label: "МНН", modalType: .mnn, binding: $mnnBinding, isPresented: $mnnModal, handler: tapMNN),
+            SearchItem(icon: "pills", label: "Лекарственная форма", modalType: .form, binding: $formBinding, isPresented: $formModal, handler: tapForm),
+            SearchItem(icon: "eye", label: "АТХ", modalType: .atc, binding: $atcBinding, isPresented: $atcModal, handler: tapATC),
         ]
     }
     
@@ -67,19 +67,69 @@ extension SearchView {
     }
 }
 
+enum ModalType {
+    case country
+    case mnn
+    case form
+    case atc
+}
+
+struct TextModel {
+    let id: Int
+    let name: String
+    let fullname: String
+}
+
 struct ModalView: View {
     @Binding var presentedAsModal: Bool
+    let modalType: ModalType
     @State var textBinding: String = ""
     @State var hasFocus: Bool = false
+    @State var items: [TextModel] = []
+    
+    @State var url: String = ""
     
     var body: some View {
-        VStack {
+        VStack(spacing: 15) {
             freewordSubview
             
+            ScrollView {
+                ForEach(items, id: \.id) { model in
+                    HStack(spacing: 20) {
+                        Image(systemName: "checkmark.square")
+                            .onTapGesture {
+                                withAnimation(.easeInOut(duration: 0.5)) {
+                                    self.presentedAsModal = false
+                                }
+                            }
+                        Text(model.name)
+                        
+                        Spacer()
+                    }
+                    .padding(.bottom, 15)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                }
+            }
             
+            Spacer()
         }
         .padding()
-//        Button("dismiss") { self.presentedAsModal = false }
+        .onAppear {
+            switch modalType {
+            case .country:
+                url = K.API.COUNTRY_LIST
+                fetchItems(type: [CountryModel].self)
+            case .mnn:
+                url = K.API.MNN_LIST
+                fetchItems(type: [MnnModel].self)
+            case .form:
+                url = K.API.FORM_LIST
+                fetchItems(type: [FormModel].self)
+            case .atc:
+                url = K.API.ATC_LIST
+                fetchItems(type: [AtcModel].self)
+            }
+        }
     }
     
     private var freewordSubview: some View {
@@ -115,11 +165,57 @@ struct ModalView: View {
             RoundedRectangle(cornerRadius: 15).stroke(Color(.systemGray), lineWidth: 2)
         )
     }
+    
+    private func fetchItems<T: Codable>(type: T.Type) {
+        Api.shared.fetch(of: T.self, from: url) { result in
+            switch result {
+            case .failure(let error):
+                print("[FAILED]: " + url)
+                print("\(error.localizedDescription)")
+            case .success(let data):
+                DispatchQueue.main.async {
+//                    Files.shared.write(filename: K.Source.COATE, item: data)
+//                    self.coate.data = data
+                    print("[SUCCESS]: \(data)")
+                    add(data)
+                }
+            }
+        }
+    }
+    
+    private func add(_ data: Decodable & Encodable) {
+        switch modalType {
+        case .country:
+            if let items = data as? [CountryModel] {
+                for item in items {
+                    self.items.append(TextModel(id: item.id, name: item.country_name, fullname: item.country_fullname))
+                }
+            }
+        case .mnn:
+            if let items = data as? [MnnModel] {
+                for item in items {
+                    self.items.append(TextModel(id: item.id, name: item.mnn_name, fullname: ""))
+                }
+            }
+        case .form:
+            if let items = data as? [FormModel] {
+                for item in items {
+                    self.items.append(TextModel(id: item.id, name: item.full_name, fullname: item.description))
+                }
+            }
+        case .atc:
+            if let items = data as? [AtcModel] {
+                for item in items {
+                    self.items.append(TextModel(id: item.id, name: "\(item.atc_code) \(item.atc_name_rus)", fullname: ""))
+                }
+            }
+        }
+    }
 }
 
 struct ModalView_Previews: PreviewProvider {
     static var previews: some View {
-        ModalView(presentedAsModal: .constant(false))
+        ModalView(presentedAsModal: .constant(false), modalType: .country)
     }
 }
 
@@ -128,6 +224,7 @@ struct SearchItem: View {
     let id: String = UUID().uuidString
     let icon: String
     let label: String
+    let modalType: ModalType
     @Binding var binding: String
     @Binding var isPresented: Bool
 //    let handler: (Binding<String>) -> ()
@@ -152,7 +249,7 @@ struct SearchItem: View {
                 .strokeBorder(Color(uiColor: UIColor.systemGray3), style: StrokeStyle(lineWidth: 0.5))
         )
         .background(RoundedRectangle(cornerRadius: 10.0).fill(Color(.systemBackground)))
-        .sheet(isPresented: $isPresented) { ModalView(presentedAsModal: self.$isPresented) }
+        .sheet(isPresented: $isPresented) { ModalView(presentedAsModal: self.$isPresented, modalType: modalType) }
         .onTapGesture {
             handler()
             isPresented = true
@@ -162,7 +259,7 @@ struct SearchItem: View {
 
 struct SearchItem_Previews: PreviewProvider {
     static var previews: some View {
-        SearchItem(icon: "number", label: "Страна производителя", binding: .constant("Test text"), isPresented: .constant(false)) {
+        SearchItem(icon: "number", label: "Страна производителя", modalType: .country, binding: .constant("Test text"), isPresented: .constant(false)) {
         }
         .previewLayout(.sizeThatFits)
     }
