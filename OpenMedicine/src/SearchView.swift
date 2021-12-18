5912cba0380e090abc48fc2e3d0b3c12d2e08bc9
Nurlan <nurlan.kalybekov@pricer.co.jp>
2021-12-18 12:04:57 +0600

    Add totalNumber and Search button to SearchView

--- OpenMedicine/src/SearchView.swift
+++ OpenMedicine/src/SearchView.swift
@@ -20,16 +20,59 @@ struct SearchView: View {
     @State var formModal: Bool = false
     @State var atcModal: Bool = false
     
+    // variables
+    private var blueColor = Color(.sRGB, red: 0, green: 0.64, blue: 1, opacity: 1)
+    
     var body: some View {
         VStack(spacing: 10) {
+            Spacer()
+            totalSubview
+            Spacer()
             ForEach(items, id: \.id) { item in
                 item
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
             }
+            Spacer()
+            Button {
+                
+            } label: {
+                Text("ПОИСК")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(.systemBackground))
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20).stroke(Color(.systemBackground), lineWidth: 2)
+                    )
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(red: 0, green: 164/255, blue: 255/255))
+                    )
+            }
+            
+            Spacer()
+            Spacer()
         }
         .padding(.horizontal)
         .onAppear {
             initItems()
+        }
+    }
+    
+    private var totalSubview: some View {
+        HStack() {
+            Text("Найдено: ")
+                .font(.system(size: 30))
+            Text("5300")
+                .font(.system(size: 40))
+                .foregroundColor(blueColor)
+                .padding(.trailing, 5)
+            Image("pill")
+                .resizable()
+                .renderingMode(.original)
+                .frame(width: 36, height: 36)
+            
+            Spacer()
         }
     }
 }
